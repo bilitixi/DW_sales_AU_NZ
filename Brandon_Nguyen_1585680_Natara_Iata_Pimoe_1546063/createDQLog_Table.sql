2b65dcf0cd2cff0483f746c3181ae98b718a94c4
bilitixi <bilitixi@gmail.com>
2026-03-07 13:47:27 +1300

    Delete Brandon_Nguyen_1585680_Natara_Iata_Pimoe_1546063 directory
    delete old folder structure

--- Brandon_Nguyen_1585680_Natara_Iata_Pimoe_1546063/createDQLog_Table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE DQLog
-(
-LogID 		int PRIMARY KEY IDENTITY,
-RowID 		varbinary(32),		-- This is a physical address of a row stored on a disk and it is UNIQUE
-DBName 		nchar(20),
-TableName	nchar(20),
-RuleNo		smallint,
-Action		nchar(6) CHECK (action IN ('allow','fix','reject')) -- Action can be ONLY 'allow','fix','reject'
-);
--- Brandon_Nguyen_1585680_Natara_Iata_Pimoe_1546063/summary_table.sql
+++
@@ -1,3 +0,0 @@
- SELECT		TableName, RuleNo, action, COUNT(*) as TotalRecords 
- FROM		DQLog
- GROUP BY	TableName, RuleNo, action;
